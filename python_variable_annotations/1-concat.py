2e7b09935ec9969159e0dca5e00a0513a3723397
root <alfredo.445566@gmail.com>
2024-05-27 23:51:56 +0000

    add type-annotated function concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type annotated function, concatenates two strings and
+returns concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns result"""
+    return str1 + str2
