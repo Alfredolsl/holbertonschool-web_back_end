346e4320a581518e0b7b91f95a8b840e9603feea
root <alfredo.445566@gmail.com>
2024-05-27 23:43:46 +0000

    add documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""Type-annotated function add that takes a float 'a' and float 'b'."""
 def add(a : float, b : float) -> float :
+    """Returns the sum of a and b"""
     return a + b
