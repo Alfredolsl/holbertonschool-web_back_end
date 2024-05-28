a0f2dbeccde349c4a2d1c6d172b128b6ca2f1618
root <alfredo.445566@gmail.com>
2024-05-28 05:21:41 +0000

    changed import module random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,11 +4,11 @@ default value of 10) named wait_random that waits for a random delay between 0
 and max_delay (included and float value) seconds and
 eventually returns it."""
 from asyncio import sleep
-from random import random
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """Pauses based on delay's value, then returns said value."""
-    delay = max_delay * random()
+    delay = max_delay * random.random()
     await sleep(delay)
     return delay
