b5ac971c3495ff90df18c32d78a7ee63f44026e4
root <alfredo.445566@gmail.com>
2024-05-28 00:21:05 +0000

    add defining variables with type-annotation

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Define and annotate the following variables with the specified values:
+   a: integer with a value of 1
+   pi: float with value of 3.14
+   i_understand_annotations, boolean with a value of True
+   school, str with value of 'Holberton'
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
