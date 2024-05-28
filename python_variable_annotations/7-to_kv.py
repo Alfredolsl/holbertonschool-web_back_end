a7cb969889f217afc4de133b28a290ad550a3142
root <alfredo.445566@gmail.com>
2024-05-28 00:39:32 +0000

    fixed return value

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,4 +7,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple, the first element is the string 'k',
     the second element is a float of the result of square
     of the int/float 'v'"""
-    return tuple(k, v * v)
+    return tuple([k, v * v])
