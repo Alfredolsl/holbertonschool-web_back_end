a162e4e2be77df49572e9be7110590e2cc72c875
root <alfredo.445566@gmail.com>
2024-05-28 00:24:54 +0000

    add type-annotated function sum_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function sum_list"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Takes input_list and returns their sum as a float."""
+    return sum(input_list)
