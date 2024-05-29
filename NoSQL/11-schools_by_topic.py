656c2f0f645080d1282c71b1fa2b5bc9794c23a1
root <alfredo.445566@gmail.com>
2024-05-29 15:10:38 -0500

    fix typo

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -7,6 +7,6 @@ def schools_by_topic(mongo_collection, topic):
     mongo_collection will be the pymongo collection object
     topic (string) will be topic searched
     """
-    documents = mongo_collection.find({"topic": topic})
+    documents = mongo_collection.find({"topics": topic})
     list_documents = [doc for doc in documents]
     return list_documents
