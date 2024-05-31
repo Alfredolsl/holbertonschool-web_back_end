05ed4c850a7c1e063bcb86b9accf98bd4e78b06d
root <alfredo.445566@gmail.com>
2024-05-31 21:27:58 +0000

    fixed curly bracket

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,7 +7,7 @@ function handleProfileSignup() {
       const { firstName } = values[1];
       const { lastName } = values[1];
       console.log(`${body} ${firstName} ${lastName}`);
-    )}
+    })
     .catch(() => console.log('Signup system offline'));
 }
 
