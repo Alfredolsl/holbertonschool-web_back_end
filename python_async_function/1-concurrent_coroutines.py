4608627ba2d514de95d43942fbd1af1b7e0ec7e6
root <alfredo.445566@gmail.com>
2024-08-18 22:34:38 +0000

    pycodestyle changes

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,12 +10,12 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns the list of all the delays (float values)."""
     spawn_list = []
-    delayed_list = []
+    delay_list = []
 
     for task in range(n):
         delayed_task = asyncio.create_task(wait_random(max_delay))
-        delayed_task.add_done_callback(lambda x:
-                                       delayed_list.append(x.result())) # this line triggers the anonymous function when delayed_task is done.
+        # Line below triggers the anonymous function when delayed_task is done.
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
         spawn_list.append(delayed_task)
 
     for spawn in spawn_list:
