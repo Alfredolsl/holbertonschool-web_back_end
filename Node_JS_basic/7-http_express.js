304a62a36a5e2b690f141eed862efd60234d7d8a
root <alfredo.445566@gmail.com>
2024-09-20 17:09:07 +0000

    add task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+const args = process.argv.slice(2);
+const database = args[0];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(database);
+    res.write('This is the list of our students\n');
+    res.end(`${students.join('\n')}`);
+  } catch (error) {
+    res.end(error.message);
+  }
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
