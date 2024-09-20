a404bb5a802e1c8ae9392321a01188601c423a90
root <alfredo.445566@gmail.com>
2024-09-20 04:49:41 +0000

    now resolves with proper information

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -9,8 +9,13 @@ function countStudents(path) {
       }
       let content = data.toString().split('\n');
       content = content.slice(1, -1);
+      const response = [];
+      let msg;
 
-      console.log(`Number of students: ${content.length}`);
+      msg = `Number of students: ${content.length}`;
+      console.log(msg);
+      response.push(msg);
+
 
       // count number of students in specific field
       const fields = {};
@@ -27,10 +32,12 @@ function countStudents(path) {
 
       for (const key in fields) {
         if (Object.hasOwn(fields, key)) {
-          console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
+          msg = `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`;
+          console.log(msg);
+          response.push(msg);
         }
       }
-      resolve(content);
+      resolve(response);
     }); // end of readFile
   }); // end of promise
 }
